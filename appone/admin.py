73652082b01d794c8913fe80a5099b7c3073c7c2
swapnali0212 <swapnalikapare02@gmail.com>
2020-12-01 01:18:04 +0530

    all files are commited

--- appone/admin.py
+++ appone/admin.py
@@ -3,7 +3,6 @@ from django.contrib.auth.admin import UserAdmin
 from appone.models import UserProfi, Student, Teacher, Subject
 # Register your models here.
 
-# Register your models here.
 admin.site.register(UserProfi)
 admin.site.register(Student)
 admin.site.register(Teacher)
